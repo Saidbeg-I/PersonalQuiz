e5ec425d55d86f599a732bdaa468787f83fa8607
Saidbeg-I <saido1996@mail.ru>
2023-03-17 23:18:58 +1200

    1 setup application

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by brubru on 18.08.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    // 1. Передать массив с ответами на экран с результатами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результаты в соотвствии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+}
